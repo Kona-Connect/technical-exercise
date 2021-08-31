6148cbb7c479953fc18eb4bdc7f8f7f06334f3be
pmaletia <pmaletia@gmail.com>
2021-08-31 20:01:49 +0530

    mysolution

--- index.py
+++ index.py
@@ -1,58 +1,183 @@
-from __future__ import print_function
-
 import time
 from typing import Any, Dict, Optional
-
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Optional
-
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "5ed045a527d2aee3d800e3108463f35300823c322a595d44564c359f6a774037"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe": {
+        "id": 31082021184078,
+        "name": "johndoe",
+        "hashed_secret": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "age": "23",
+        "disabled": False,
+    }
+}
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(SQLModel):
+    username: Optional[str] = None
 
 class User(SQLModel, table=True):
     # TODO change the default value to the timestamp that the user is created
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=int(time.strftime("%d%m%y%H%M%S", time.localtime())), primary_key=True)
     name: str
     secret_name: str
     age: Optional[str] = None
-      
-engine = create_engine("sqlite:///database.db")
+
+
+connect_args = {"check_same_thread": False}      
+engine = create_engine("sqlite:///database.db", echo=True, connect_args=connect_args)
 SQLModel.metadata.create_all(engine)
+
 # Create an instance of the API class
 app = FastAPI()
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user1(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user1(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user1(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
 
 @app.get("/")
 async def root():
     # TODO include the user's name if they are logged in
-    return {"message": "Hello {}".format("World")}
+    return {"message": "Hello"}
 
 
 @app.post("/user")
 async def create_new_user(*, user: User):
+    print(user.id)
     with Session(engine) as session:
-      session.add(user)
-      session.commit()
-    # TODO return the User ID
-    return {"message": "User created"}
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+    return {"message": f"User {user.id} created"}
     
 @app.get("/user/{id}")
 async def get_user(id: int):
-  with Session(engine) as session:
-     # TODO return the user based on the ID (and an error if not)
-     statement = select(User).where(User.name == id)
-     user = session.exec(statement).first()
-     return {"user": user}
+    with Session(engine) as session:
+        statement = select(User).where(User.id == id)
+        user = session.exec(statement).first()
+        if user:
+            return {"user": user}
+        else:
+            return {"message" : f"error in finding the person with id {id}"}
 
 @app.get("/api/webhooks")
 async def handle_hook(*, event: Any):
     id = event.payload.conversation.id
     return {"message": "Hello World"}
-  
-
-
-
-
-
-
-
-
