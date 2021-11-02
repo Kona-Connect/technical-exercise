7d8bf5f0d8ec372bf9f43e5f5fcb90fb6bee8124
Georgia Kokkinou <geor5ko@gmail.com>
2021-11-02 14:23:33 +0200

    Apply various minor improvements
    Remove some unused imports and some redundant blank lines. Also, change
    the type of the age attribute of the User class from str to int.
    Finally, use consistent indentation of four spaces throughout the code.

--- index.py
+++ index.py
@@ -1,20 +1,17 @@
-from __future__ import print_function
-
 import time
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-
-from typing import Optional
-
 from sqlmodel import Field, Session, SQLModel, create_engine, select
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     secret_name: str
-    age: Optional[str] = None
+    age: Optional[int] = None
+
       
 engine = create_engine("sqlite:///database.db")
 SQLModel.metadata.create_all(engine)
@@ -29,35 +26,25 @@ async def root(credentials: HTTPBasicCredentials = Depends(security)):
     username = credentials.username if credentials else "World"
     return {"message": "Hello {}".format(username)}
 
-
 @app.post("/user")
 async def create_new_user(*, user: User):
     with Session(engine) as session:
-      if user.id is None:
-        user.id = int(time.time())
-      session.add(user)
-      session.commit()
-      return {"message": "User with ID {} created".format(user.id)}
+        if user.id is None:
+            user.id = int(time.time())
+        session.add(user)
+        session.commit()
+        return {"message": "User with ID {} created".format(user.id)}
     
 @app.get("/user/{id}")
 async def get_user(id: int):
-  with Session(engine) as session:
-     statement = select(User).where(User.id == id)
-     user = session.exec(statement).first()
-     if user is None:
-         raise HTTPException(status_code=404, detail="User not found")
-     return {"user": user}
+    with Session(engine) as session:
+        statement = select(User).where(User.id == id)
+        user = session.exec(statement).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return {"user": user}
 
 @app.get("/api/webhooks")
 async def handle_hook(*, event: Any):
     id = event.payload.conversation.id
     return {"message": "Hello World"}
-  
-
-
-
-
-
-
-
-
